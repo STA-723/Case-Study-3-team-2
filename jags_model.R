91828e99c747e70487a97fb22305870bf4a68a51
betsy bersson <betsybersson@gmail.com>
2020-02-16 16:45:27 -0500

    edited jags model- not using

--- jags_model.R
+++ jags_model.R
@@ -1,30 +1,46 @@
 library(R2jags)
+library(corrplot)
 
+# load in data
 load("df.rdata")
 
 df$num_of_drinks = round(df$num_of_drinks,0)
+
+# look at correlation plot
+corrplot(cor(df[,48:50]), method="pie")
+
+
+#df = df[sample(nrow(df),1000),]
 
 # OBTAIN DESIGN MATRIX/ DATA MATRICES
 des_mat = model.matrix(lm(num_of_drinks ~ ., data = df))
 
 nbmodel = function(){
   
-  ## Likelihood
   for(i in 1:N){
-    y[i] ~ dpois(lambda[i]*gam)
-    log(lambda[i]) <- mu[i]
-    mu[i] <- inprod(beta[],X[i,])
+    ## Likelihood
+    y[i] ~ dnegbin(prob[i],r)
+    prob[i] <- r/(r+(1-zero[i])*lambda.count[i]) - 1e-10*zero[i]
+    lambda.count[i] <- exp(mu.count[i])
+    mu.count[i] <- inprod(beta[],X[i,])
+    
+    ## Zero-Inflation
+    zero[i] ~ dbern(pi[i])
+    pi[i] <- ilogit(mu.binary[i])  
+    mu.binary[i] <- inprod(alpha[],X[i,])
+    
   } 
-  gam ~ dgamma(1/phi,1/phi) 
-  phi ~ dgamma(2,1)
+  r ~ dunif(0,50)
   
   ## Priors
   for ( i in 1:p){
     beta[i] ~ dnorm(mu.beta,tau.beta)
+    alpha[i] ~ dnorm(mu.alpha,tau.alpha)
   }
   
   # prior for var to approximate p(phi)=1/phi
   tau.beta ~ dgamma(.001,.001)
+  tau.alpha ~ dgamma(.001,.001)
   
 }
 
@@ -33,6 +49,7 @@ forJags = list(X=des_mat,
                y=c(df$num_of_drinks),
                N=nrow(des_mat),
                mu.beta=0,
+               mu.alpha=0,
                p = ncol(des_mat))
 
 
